954b9474b06f208687e397940a7a0809ecc2f049
Richard Abhulimhen <richardabhu@fb.com>
2022-06-23 16:11:14 -0700

    "Spotify login authorization for User completed." "Automatically redirects user to MainActivity.java"

--- app/src/main/java/com/example/richard/vybe/SplashActivity.java
+++ app/src/main/java/com/example/richard/vybe/SplashActivity.java
@@ -1,15 +1,43 @@
 package com.example.richard.vybe;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.example.richard.vybe.Swipe.MainActivity;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
-public class SplashActivity extends Activity {
+//public class SplashActivity extends Activity {
+//
+//    private SharedPreferences.Editor editor;
+//    private SharedPreferences msharedPreferences;
+//
+//    private RequestQueue queue;
+//
+//    private static final String CLIENT_ID = "4b2696ee4bdc43b6aeb2c828e76b9374";
+//    private static final String REDIRECT_URI = "com.example.richard.vybe://callback";
+//    private static final int REQUEST_CODE = 1337;
+//
+//    private String TAG = "SplashActivity";
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        setContentView(R.layout.activity_splash);
+//
+//    }
+//}
+
+public class SplashActivity extends AppCompatActivity {
 
     private SharedPreferences.Editor editor;
     private SharedPreferences msharedPreferences;
@@ -26,8 +54,76 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
 
+
+        Log.d(TAG, "JUST STARTING");
+        authenticateSpotify();
+        Log.d(TAG, "FINISHED AUTHENTICATING");
+
+        msharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        queue = Volley.newRequestQueue(this);
     }
+
+
+    private void waitForUserInfo() {
+        startMainActivity();
+    }
+
+    private void startMainActivity() {
+        Intent newintent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(newintent);
+    }
+
+
+    private void authenticateSpotify() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-recently-played,user-library-modify,user-library-read,playlist-modify-public,playlist-modify-private,user-read-email,user-read-private,playlist-read-private,playlist-read-collaborative"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        Log.d(TAG, "START ACTIVITY RESULT");
+        Log.d(TAG, "RESULT CODE: " + resultCode);
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            Log.d(TAG, "FINISHED REQUEST CODE");
+            Log.d(TAG, "RESULT TYPE" + response.getType().toString());
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    editor = getSharedPreferences("SPOTIFY", 0).edit();
+                    editor.putString("token", response.getAccessToken());
+                    Log.d(TAG, "GOT AUTH TOKEN");
+                    editor.apply();
+                    Log.i(TAG, "GO TO NEW PAGE");
+                    waitForUserInfo();
+                    Log.i(TAG, "Gone");
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    Log.d(TAG, "ERROR IN SWITCH CASE: " + response.getError());
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+                    Log.d(TAG, "DEFAULT IN SWITCH CASE");
+            }
+        }
+    }
+
+
 }
+
 
--- app/src/main/java/com/example/richard/vybe/Swipe/MainActivity.java
+++ app/src/main/java/com/example/richard/vybe/Swipe/MainActivity.java
@@ -1,6 +1,25 @@
 package com.example.richard.vybe.Swipe;
 
-import android.app.Activity;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.richard.vybe.R;
+
+
+public class MainActivity extends FragmentActivity {
+
+    private String TAG = "MainActivity";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
 }
