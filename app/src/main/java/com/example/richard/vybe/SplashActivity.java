c6f942e24001238064e379c7e66ef6446ce52ce7
Richard Abhulimhen <richardabhu@fb.com>
2022-06-23 13:55:38 -0700

    "Configured Spotify API and added Client_ID and REDIRECT_URL."

--- app/src/main/java/com/example/richard/vybe/SplashActivity.java
+++ app/src/main/java/com/example/richard/vybe/SplashActivity.java
@@ -1,13 +1,26 @@
 package com.example.richard.vybe;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
 public class SplashActivity extends Activity {
+
+    private SharedPreferences.Editor editor;
+    private SharedPreferences msharedPreferences;
+
+    private RequestQueue queue;
+
+    private static final String CLIENT_ID = "4b2696ee4bdc43b6aeb2c828e76b9374";
+    private static final String REDIRECT_URI = "com.example.richard.vybe://callback";
+    private static final int REQUEST_CODE = 1337;
+
+    private String TAG = "SplashActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
