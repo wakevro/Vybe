1a4229992d783ff0b01da83fab32fefb1a43da4a
Richard Abhulimhen <richardabhu@fb.com>
2022-07-06 11:25:05 -0700

    "Added Home Fragment to display Users playlists and each playlists display all songs in it when clicked."

--- app/src/main/java/com/example/richard/vybe/Home/HomeFragment.java
+++ app/src/main/java/com/example/richard/vybe/Home/HomeFragment.java
@@ -1,11 +1,97 @@
 package com.example.richard.vybe.Home;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.example.richard.vybe.Model.Playlist;
+import com.example.richard.vybe.Model.User;
+import com.example.richard.vybe.R;
+import com.example.richard.vybe.SpotifyConnect.PlaylistService;
+import com.example.richard.vybe.SpotifyConnect.UserService;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
+    private String TAG = "HomeFragment";
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private RequestQueue requestQueue;
+    private ArrayList<Playlist> playlists = new ArrayList<>();
+    private SharedPreferences sharedPreferences;
+    private PlaylistService playlistService;
+
+    String userProfileImageURL;
+    private User user;
+    private TextView tvUsername;
+    private ImageView ivProfileImage;
+
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_home, container, false);
+
+        sharedPreferences = getContext().getSharedPreferences("SPOTIFY", 0);
+        requestQueue = Volley.newRequestQueue(getContext());
+        playlistService = new PlaylistService(requestQueue, sharedPreferences);
+
+        recyclerView = rootView.findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(false);
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        tvUsername = rootView.findViewById(R.id.tvUsername);
+        ivProfileImage = rootView.findViewById(R.id.ivProfileHome);
+
+
+
+        UserService userService = new UserService(requestQueue, sharedPreferences);
+        Log.i(TAG, "GOTTEN USER SERVICE.");
+        userService.get(() -> {
+            Log.i(TAG, "STARTING TO GET USER.");
+            User user = userService.getUser();
+            Log.i(TAG, "USER NAME: " + user.getDisplay_name());
+
+            Log.i(TAG, "IMAGE: " + user.getProfileImageURL());
+
+            String userName = user.getDisplay_name().split(" ")[0];
+            userProfileImageURL = user.getProfileImageURL();
+            tvUsername.setText(userName);
+
+            if (userProfileImageURL == null) {
+                Glide.with(getContext())
+                        .load(R.drawable.man)
+                        .transform(new CircleCrop())
+                        .into(ivProfileImage);
+            } else {
+                Glide.with(getContext())
+                        .load(userProfileImageURL)
+                        .transform(new CircleCrop())
+                        .into(ivProfileImage);
+            }
+
+
+        });
+        return rootView;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,10 +107,14 @@ public class HomeFragment extends Fragment {
     }
 
     public void getPlaylists() {
-
+        playlists.clear();
+        playlistService.get(() -> grabPlaylist());
     }
 
     public void grabPlaylist() {
-
+        playlists = playlistService.getPlaylists();
+        mAdapter = new PlaylistAdapter(playlists, getContext());
+        mAdapter.notifyDataSetChanged();
+        recyclerView.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/com/example/richard/vybe/Home/PlaylistAdapter.java
+++ app/src/main/java/com/example/richard/vybe/Home/PlaylistAdapter.java
@@ -1,32 +1,114 @@
 package com.example.richard.vybe.Home;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.richard.vybe.Model.Playlist;
+import com.example.richard.vybe.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.MyViewHolder> {
 
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    private String TAG = "PlaylistAdapter";
+
+    private ArrayList<Playlist> mDataset;
+    private Context mContext;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView tvPlaylistName;
+        public TextView tvPlaylistSongSize;
+        public ImageView ivPlaylistImage;
+        private DatabaseReference databaseReference;
+        private SharedPreferences.Editor editor;
+        private SharedPreferences sharedPreferences;
+        private Playlist playlist;
+        private Context context;
+
+        public MyViewHolder(View itemView, Context context) {
+            super(itemView);
+
+            this.context = context;
+            sharedPreferences = context.getSharedPreferences("SPOTIFY", 0);
+            databaseReference = FirebaseDatabase.getInstance().getReference().child(sharedPreferences.getString("username", "") + " " + sharedPreferences.getString("userid", ""));
+
+
+            itemView.setOnClickListener(this);
+
+            tvPlaylistName = itemView.findViewById(R.id.tvPlaylistName);
+            tvPlaylistSongSize = itemView.findViewById(R.id.tvPlaylistSongSize);
+            ivPlaylistImage = itemView.findViewById(R.id.ivPlaylistImage);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(context, "Playlist " + playlist.getName() + " selected", Toast.LENGTH_SHORT).show();
+
+            databaseReference.child("Playlist").child("id").setValue(playlist.getId());
+            databaseReference.child("Playlist").child("name").setValue(playlist.getName());
+            databaseReference.child("Playlist").child("tracks").setValue(playlist.getTotalTracks());
+
+            editor = sharedPreferences.edit();
+            editor.putString("playlist", playlist.getId());
+            editor.putString("playlistname", playlist.getName());
+            editor.apply();
+
+            Intent intent = new Intent(context, PlaylistItemsActivity.class);
+            context.startActivity(intent
+            );
+
+        }
+
+
+    }
+
+    public PlaylistAdapter(ArrayList<Playlist> myDataset, Context context) {
+        mDataset = myDataset;
+        mContext = context;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                           int viewType) {
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.playlist, null, false);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutView.setLayoutParams(lp);
 
+        MyViewHolder vh = new MyViewHolder(layoutView, mContext);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        Log.i(TAG, "PLAYLIST DICTIONARY: "+ mDataset.get(position));
+        holder.tvPlaylistName.setText(mDataset.get(position).getName());
+        holder.playlist = mDataset.get(position);
+        holder.tvPlaylistSongSize.setText(Integer.toString(holder.playlist.getTotalTracks()) + " songs");
+        if (holder.playlist.getImageURL() != null) {
+            Glide.with(mContext)
+                    .load(mDataset.get(position).getImageURL())
+                    .transform(new RoundedCorners(30))
+                    .into(holder.ivPlaylistImage);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
-    }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public MyViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
+        return mDataset.size();
     }
 }
